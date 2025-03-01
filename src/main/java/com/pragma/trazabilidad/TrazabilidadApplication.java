86af308426ea0e40e7b045c2a1b19029423e394f
yafernandez <yeferson1500@gmail.com>
2025-03-01 18:59:07 -0500

    chore: initial commit from skeleton

---
+++ src/main/java/com/pragma/trazabilidad/TrazabilidadApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.trazabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrazabilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrazabilidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/trazabilidad/TrazabilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.trazabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrazabilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
